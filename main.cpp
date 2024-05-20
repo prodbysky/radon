a1748722be60fbf19ac0203ab73ae9f2516be85d
prodbysky <kyrbynyt@gmail.com>
2024-05-20 21:38:13 +0300

    Move out types

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-#include <cstdint>
+#include "types.h"
+
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <vector>
-
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u64 = uint64_t;
 
 enum class TokenType { Number, BinaryOperator };
 
